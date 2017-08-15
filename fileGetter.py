fe568c96bf445bb97c7a47776d8f75db05d319cc
NVRC <nathaniel.charlebois@gmail.com>
2017-08-15 16:41:14 -0700

    Added directory choosers.

--- fileGetter.py
+++ fileGetter.py
@@ -1,19 +1,22 @@
 import shutil, os
+from Tkinter import Tk
+from tkFileDialog import askdirectory
 
 #This utility parses the approved JPG folder and copies the respective
 #RAW into the designated folder
 
 #Useful when sorting large numbers of JPGs then copying the RAWs to later polish
 
-#Filepath of the JPG folder (TITLE SRC)
-filePathJPG = '/Users/Slate/Documents/Photos/Fuji/Vineyard/JPG'
-#Filepath of the RAW folder (IMG SRC)
-filePathRaw = '/Volumes/Untitled/DCIM/108_FUJI'
-#Filepath of the RAW folder (IMD DEST)
-filePathRawDEST = '/Users/Slate/Documents/Photos/Fuji/Vineyard/RAW'
-
 filenames = []
 suffix = '.RAF'
+
+Tk().withdraw()
+#Filepath of the JPG folder (TITLE SRC)
+filePathJPG = askdirectory(title='Select the JPG folder')
+#Filepath of the RAW folder (IMG SRC)
+filePathRaw = askdirectory(title='Select the source folder with the RAWs')
+#Filepath of the RAW folder (IMD DEST)
+filePathRawDEST = askdirectory(title='Select the RAWs destination folder')
 
 for file in os.listdir(filePathJPG):
     if file.endswith(".JPG"):
@@ -21,4 +24,3 @@ for file in os.listdir(filePathJPG):
 
 for name in filenames:
     shutil.copy2(os.path.join(filePathRaw,name+suffix),filePathRawDEST)
-    print(os.path.join(filePathRawDEST,name + suffix))
